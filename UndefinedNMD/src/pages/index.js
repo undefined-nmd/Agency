cec53e958f8c30a0fd28457b13278ac96cf17a92
gdm-1718-brundeco <32094744+gdm-1718-brundeco@users.noreply.github.com>
2019-10-14 11:44:16 +0200

    Project page ready

--- UndefinedNMD/src/pages/index.js
+++ UndefinedNMD/src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
-import "../components/assignments.css"
+import "../components/index.css"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -22,7 +22,12 @@ const IndexPage = () => (
           </div>
           <div className={'content-div'}>
               <div className={'content-child'}>
-
+                  <div className="toggle-nav">
+                      <div className="nav-line-top"></div>
+                      <div className="nav-line-bottom"></div>
+                  </div>
+                    <h2 className="brand-title">_defined</h2>
+                    <h4 className="baseline">We <span>Build</span> <span>Digital</span> <span>Stuff</span></h4>
               </div>
           </div>
       </div>
