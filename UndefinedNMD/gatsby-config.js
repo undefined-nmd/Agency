1de533cf7ae106324f443ad9878880e978b01de3
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-10-14 13:56:36 +0200

    Revert "firebase tegration"
    This reverts commit 32d0824fcc67c8638e95d9b5e1ee0e4b911265d8.
    
    # Conflicts:
    #	UndefinedNMD/src/assets/images/profiles/anna.jpg
    #	UndefinedNMD/src/assets/images/profiles/ben.jpg
    #	UndefinedNMD/src/assets/images/profiles/bruno.jpg
    #	UndefinedNMD/src/assets/images/profiles/cedric.jpg
    #	UndefinedNMD/src/assets/images/profiles/hannes.jpg
    #	UndefinedNMD/src/assets/images/profiles/hektor.jpg
    #	UndefinedNMD/src/assets/images/profiles/jerome.jpg
    #	UndefinedNMD/src/assets/images/profiles/jonas.jpg
    #	UndefinedNMD/src/assets/images/profiles/juan.jpg
    #	UndefinedNMD/src/assets/images/profiles/lukas.jpg
    #	UndefinedNMD/src/assets/images/profiles/nawang.jpg
    #	UndefinedNMD/src/assets/images/profiles/ona.jpg
    #	UndefinedNMD/src/assets/images/profiles/quinten.jpg
    #	UndefinedNMD/src/assets/images/profiles/thor.jpg
    #	UndefinedNMD/src/assets/images/profiles/wouter.jpg
    #	UndefinedNMD/src/assets/images/profiles/yoram.jpg

--- UndefinedNMD/gatsby-config.js
+++ UndefinedNMD/gatsby-config.js
@@ -28,6 +28,59 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-firebase`,
+      options: {
+        // point to the firebase private key downloaded
+        credential: require("./firebase-key.json"),
+
+        // your firebase database root url
+        databaseURL: "https://reactjs-training.firebaseio.com",
+
+        // you can have multiple "types" that point to different paths
+        types: [
+          {
+            // this type will become `allWorkshop` in graphql
+            type: "Workshop",
+
+            // the path to get the records from
+            path: "v2/data/workshops",
+
+            // probably don't want your entire database, use the query option
+            // to limit however you'd like
+            query: ref => ref.limitToLast(10),
+
+            // This allows you to map your data to data that GraphQL likes:
+            // 1. Turn your lists into actual arrays
+            // 2. Fix keys that GraphQL hates. It doesn't allow number keys
+            //    like "0", you'll get this error pretty often:
+            //    Error: Names must match /^[_a-zA-Z][_a-zA-Z0-9]*$/ but "0" does not
+            // 3. Remove stuff you don't need.
+            //
+            // Feel free to mutate, we sent you a copy anyway.
+            map: node => {
+              // fix keys graphql hates
+              node.nineteenEightyFive = node['1985']
+              delete node['1985']
+
+              // convert a child list to an array:
+              return node.sessions = Object.keys(node.sessions).map(key => {
+                return { _key: key, session: node.sessions[key] }
+              })
+
+              // finally, return the node
+              return node
+            },
+          },
+
+          // if you're data is really simple, this should be fine too
+          {
+            type: "CourseDescriptions",
+            path: "text/courseDescriptions",
+          }
+        ]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- UndefinedNMD/src/pages/team.js
+++ UndefinedNMD/src/pages/team.js
@@ -1,73 +1,53 @@
-import React, { Component } from "react"
+import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import '../components/team.css'
 import SEO from "../components/seo"
-import {getFirebase} from "../components/firebase-config";
 
-class AssignmentsPage extends Component {
-
-    componentDidMount() {
-        const lazyApp = import('firebase/app')
-        const lazyDatabase = import('firebase/database')
-
-        Promise.all([lazyApp, lazyDatabase]).then(([firebase]) => {
-            const database = getFirebase(firebase).database()
-            database.ref('peoples').on('value', (snapshot) => {
-                snapshot.forEach((childSnapshot) => {
-                    console.log(childSnapshot.val())
-                })
-            })
-        })
-    }
-
-    render() {
-        return (
-            <Layout>
-                <SEO title="Assignments" />
-                <div className={'hero-img-detail'}>
-                    <h1 className="project-title">Our Team</h1>
-                    <div className={'subtitles-detail'}>
-                        <h5 className={'subtitle-text-detail'}>By undefined</h5>
-                        <div className={'line-detail'}></div>
-                        <h5 className={'date-text-detail'}>2019</h5>
+const AssignmentsPage = () => (
+  <Layout>
+    <SEO title="Assignments" />
+        <div className={'hero-img-detail'}>
+            <h1 className="project-title">Our Team</h1>
+            <div className={'subtitles-detail'}>
+                <h5 className={'subtitle-text-detail'}>By undefined</h5>
+                <div className={'line-detail'}></div>
+                <h5 className={'date-text-detail'}>2019</h5>
+            </div>
+            <div>HERE KOMeS DE NAV</div>
+            <div className={'content-div-detail'}>
+                    <h1>This is our peoples</h1>
+                <div className={'team-list-div'}>
+                    <div className={'person-div'}>
+                        <h3 className={'person-name'}>Jonas Stasseyns</h3>
+                        <img className={'person-image'}/>
                     </div>
-                    <div>HERE KOMeS DE NAV</div>
-                    <div className={'content-div-detail'}>
-                        <h1>This is our peoples</h1>
-                        <div className={'team-list-div'}>
-                            <div className={'person-div'}>
-                                <h3 className={'person-name'}>Jonas Stasseyns</h3>
-                                <img className={'person-image'}/>
-                            </div>
-                            <div className={'person-div'}>
-                                <h3 className={'person-name'}>Jonas Stasseyns</h3>
-                                <img className={'person-image'}/>
-                            </div>
-                            <div className={'person-div'}>
-                                <h3 className={'person-name'}>Jonas Stasseyns</h3>
-                                <img className={'person-image'}/>
-                            </div>
-                            <div className={'person-div'}>
-                                <h3 className={'person-name'}>Jonas Stasseyns</h3>
-                                <img className={'person-image'}/>
-                            </div>
-                            <div className={'person-div'}>
-                                <h3 className={'person-name'}>Jonas Stasseyns</h3>
-                                <img className={'person-image'}/>
-                            </div>
-                            <div className={'person-div'}>
-                                <h3 className={'person-name'}>Jonas Stasseyns</h3>
-                                <img className={'person-image'}/>
-                            </div>
-                        </div>
+                    <div className={'person-div'}>
+                        <h3 className={'person-name'}>Jonas Stasseyns</h3>
+                        <img className={'person-image'}/>
+                    </div>
+                    <div className={'person-div'}>
+                        <h3 className={'person-name'}>Jonas Stasseyns</h3>
+                        <img className={'person-image'}/>
+                    </div>
+                    <div className={'person-div'}>
+                        <h3 className={'person-name'}>Jonas Stasseyns</h3>
+                        <img className={'person-image'}/>
+                    </div>
+                    <div className={'person-div'}>
+                        <h3 className={'person-name'}>Jonas Stasseyns</h3>
+                        <img className={'person-image'}/>
+                    </div>
+                    <div className={'person-div'}>
+                        <h3 className={'person-name'}>Jonas Stasseyns</h3>
+                        <img className={'person-image'}/>
                     </div>
                 </div>
-            </Layout>
-        )
-    }
-}
+            </div>
+        </div>
+  </Layout>
+)
 
 export default AssignmentsPage
