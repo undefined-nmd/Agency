5794834b40c41519fc8b7330a34bae1415fa155a
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-10-14 10:51:15 +0200

    assignments skeleton

--- UndefinedNMD/src/pages/assignments.js
+++ UndefinedNMD/src/pages/assignments.js
@@ -8,7 +8,30 @@ import SEO from "../components/seo"
 const AssignmentsPage = () => (
   <Layout>
     <SEO title="Assignments" />
-
+        <div className={'hero-img-detail'}>
+            <h1>Our current assignments</h1>
+            <div className={'subtitles-detail'}>
+                <h5 className={'subtitle-text-detail'}>By undefined</h5>
+                <div className={'line-detail'}></div>
+                <h5 className={'date-text-detail'}>2019</h5>
+            </div>
+            <div>HERE KOMeS DE NAV</div>
+            <div className={'content-div-detail'}>
+                <div className={'intro-div-detail'}>
+                    <p className={'intro-text-detail'}>
+                        Here is a list of the current projects we are working on at undefined! This sentence has no purpose whatsoever, except maybe for filling this paragraph because one sentence is not quite useful for styling a p-tag.
+                    </p>
+                    <div className={'project-div'}>
+                        <h1 className={'project-title'}>Agency website</h1>
+                        <p className={'project-description'}>By the agency-website, we mean the actual website is our agency 'undefined', or, this website. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut nesciunt porro quibusdam ut. Architecto, corporis deserunt dolor doloribus inventore iusto omnis repellendus! Ad consequatur explicabo maxime quo sed unde vero!</p>
+                    </div>
+                    <div className={'project-div'}>
+                        <h1 className={'project-title'}>Sleepwalking-project</h1>
+                        <p className={'project-description'}>Another main-project of ours is the development of a solution to a sleepwalking child getting out of bed and jumping through their window. We are developing a solution that prevent this from happening.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
   </Layout>
 )
 
