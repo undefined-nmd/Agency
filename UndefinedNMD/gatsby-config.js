ecd30e6003689d63b02f6480b52a71442ca6ad5f
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-10-14 12:21:04 +0200

    updateconfig

--- UndefinedNMD/gatsby-config.js
+++ UndefinedNMD/gatsby-config.js
@@ -29,19 +29,54 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-mysql`,
+      resolve: `gatsby-source-firebase`,
       options: {
-        connectionDetails: {
-          host: 'localhost',
-          user: 'root',
-          password: 'root',
-          database: 'undefined_agency'
-        },
-        queries: [
+        // point to the firebase private key downloaded
+        credential: require("./firebase-key.json"),
+
+        // your firebase database root url
+        databaseURL: "https://reactjs-training.firebaseio.com",
+
+        // you can have multiple "types" that point to different paths
+        types: [
           {
-            statement: 'SELECT * FROM people',
-            idFieldName: 'Code',
-            name: 'person'
+            // this type will become `allWorkshop` in graphql
+            type: "Workshop",
+
+            // the path to get the records from
+            path: "v2/data/workshops",
+
+            // probably don't want your entire database, use the query option
+            // to limit however you'd like
+            query: ref => ref.limitToLast(10),
+
+            // This allows you to map your data to data that GraphQL likes:
+            // 1. Turn your lists into actual arrays
+            // 2. Fix keys that GraphQL hates. It doesn't allow number keys
+            //    like "0", you'll get this error pretty often:
+            //    Error: Names must match /^[_a-zA-Z][_a-zA-Z0-9]*$/ but "0" does not
+            // 3. Remove stuff you don't need.
+            //
+            // Feel free to mutate, we sent you a copy anyway.
+            map: node => {
+              // fix keys graphql hates
+              node.nineteenEightyFive = node['1985']
+              delete node['1985']
+
+              // convert a child list to an array:
+              return node.sessions = Object.keys(node.sessions).map(key => {
+                return { _key: key, session: node.sessions[key] }
+              })
+
+              // finally, return the node
+              return node
+            },
+          },
+
+          // if you're data is really simple, this should be fine too
+          {
+            type: "CourseDescriptions",
+            path: "text/courseDescriptions",
           }
         ]
       }
