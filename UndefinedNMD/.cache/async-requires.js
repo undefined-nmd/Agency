2957e4cb5cf2a7936780bcc756f298da97dbc0e7
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-11-05 16:26:05 +0100

    gitignore

--- UndefinedNMD/.cache/async-requires.js
+++ UndefinedNMD/.cache/async-requires.js
@@ -2,17 +2,15 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-contact-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
-  "component---src-pages-index-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-page-2-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
-  "component---src-pages-projectdetailpage-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/src/pages/projectdetailpage.js" /* webpackChunkName: "component---src-pages-projectdetailpage-js" */),
-  "component---src-pages-projects-page-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/src/pages/projectsPage.js" /* webpackChunkName: "component---src-pages-projects-page-js" */),
-  "component---src-pages-schedule-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/src/pages/schedule.js" /* webpackChunkName: "component---src-pages-schedule-js" */),
-  "component---src-pages-team-page-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/src/pages/teamPage.js" /* webpackChunkName: "component---src-pages-team-page-js" */),
-  "component---src-pages-projectspage-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/src/pages/projectspage.js" /* webpackChunkName: "component---src-pages-projectspage-js" */),
-  "component---src-pages-about-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-services-js": () => import("/Users/brunodecoene/Documents/GitHub/Agency/UndefinedNMD/src/pages/services.js" /* webpackChunkName: "component---src-pages-services-js" */)
+  "component---cache-dev-404-page-js": () => import("dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-contact-js": () => import("../src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
+  "component---src-pages-schedule-js": () => import("../src/pages/schedule.js" /* webpackChunkName: "component---src-pages-schedule-js" */),
+  "component---src-pages-services-js": () => import("../src/pages/services.js" /* webpackChunkName: "component---src-pages-services-js" */),
+  "component---src-pages-team-page-js": () => import("../src/pages/teamPage.js" /* webpackChunkName: "component---src-pages-team-page-js" */),
+  "component---src-pages-about-js": () => import("../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-projectspage-js": () => import("../src/pages/projectspage.js" /* webpackChunkName: "component---src-pages-projectspage-js" */)
 }
 
