241212e56efa0fbd0d3478efd22c8c5a992b3b8f
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-11-18 14:21:42 +0100

    Updates

---
+++ component---src-pages-page-2-js-437387a105c685e27c78.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{245:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(152),c=a(237),r=a(238);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-437387a105c685e27c78.js.map
---
+++ styles-1463c752ebdbd4925ec9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{252:function(n,o,c){},253:function(n,o,c){},254:function(n,o,c){},255:function(n,o,c){},260:function(n,o,c){},262:function(n,o,c){}}]);
+//# sourceMappingURL=styles-1463c752ebdbd4925ec9.js.map
