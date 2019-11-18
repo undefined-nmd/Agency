4a6c7bdc4c44b5424b0e59033111c0ef11ae354f
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-11-18 13:39:04 +0100

    Updates

---
+++ component---src-pages-page-2-js-b8099a9e7285ed9f27ec.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{246:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(79),c=a(239),r=a(238);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-b8099a9e7285ed9f27ec.js.map
