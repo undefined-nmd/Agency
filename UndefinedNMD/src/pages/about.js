7aeb5d87923f675d70704e4e51bd8f18bf742b82
gdm-1718-brundeco <32094744+gdm-1718-brundeco@users.noreply.github.com>
2020-01-12 20:03:58 +0100

    Footer component , Ux improvements , responsive

--- UndefinedNMD/src/pages/about.js
+++ UndefinedNMD/src/pages/about.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import Footer from "../components/footer"
 import Menu from '../components/menu'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -38,7 +39,7 @@ class About extends Component {
                 <SEO title="Assignments" />
                 <Menu menustate={this.state.navActive} toggleNav={this.getNavState} />
                 <div className={'contact-container'}>
-                    <div className="main-container">
+                    <div className="main-container-edit">
                         <div className="toggle-nav reduce-width-float add-fixed" onClick={this.getNavState}>
                             <div className="nav-line-top"></div>
                             <div className="nav-line-bottom"></div>
@@ -46,15 +47,16 @@ class About extends Component {
                         <p className="project-intro">
                             {this.state.content[0] ? this.state.content[0].data.synopsis : 'Rendering...'}
                         </p>
-                        <div className="content-box">
+                        <div className="content-box-edit">
                             <h3 className="project-subtitle">{this.state.content[0] ? this.state.content[0].data.title : 'Rendering...'}</h3>
                             <p className="project-text">
                                 {this.state.content[0] ? this.state.content[0].data.body : 'Rendering...'}
                             </p>
                         </div>
                     </div>
+                    <Footer style={'footer-border'} />
                 </div>
-                <a href="/#" className="home-btn">_defined</a>
+                {/* <a href="/#" className="home-btn">_defined</a> */}
             </Layout>
         )
     }
